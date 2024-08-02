c0b530a9cf7cc2f9c2fda4faa47daa328434d912
ykking-0516 <156627183+ykking-0516@users.noreply.github.com>
2024-08-02 15:39:26 +0800

    Update dao.py

--- src/thcloud/dao.py
+++ src/thcloud/dao.py
@@ -3,8 +3,38 @@ from typing import Generic, List
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
-from thcloud.models import Article
-from thcloud.schemas import CreateSchema, ModelType, UpdateSchema, CreateArticleSchema, UpdateArticleSchema
+from thcloud.models import (
+    Bid_catalog,
+    Bid_catalog_content,
+    Requirement_analysis,
+    Requirement_analysis_detail,
+    Response_indicator,
+    Response_indicator_detail,
+    Scheme,
+    System_framework,
+    System_framework_detail,
+)
+from thcloud.schemas import (
+    CreateBidCatalog,
+    CreateBidCatalogContent,
+    CreateRequirementAnalysis,
+    CreateRequirementAnalysisDetail,
+    CreateResponseIndicator,
+    CreateResponseIndicatorDetail,
+    CreateSchema,
+    CreateScheme,
+    CreateSystemFramework,
+    ModelType,
+    UpdateBidCatalog,
+    UpdateBidCatalogContent,
+    UpdateRequirementAnalysis,
+    UpdateRequirementAnalysisDetail,
+    UpdateResponseIndicator,
+    UpdateResponseIndicatorDetail,
+    UpdateSchema,
+    UpdateScheme,
+    UpdateSystemFramework,
+)
 
 
 class BaseDAO(Generic[ModelType, CreateSchema, UpdateSchema]):
@@ -14,7 +44,11 @@ class BaseDAO(Generic[ModelType, CreateSchema, UpdateSchema]):
         result = session.query(self.model).offset(offset).limit(limit).all()
         return result
 
-    def get_by_id(self, session: Session, pk: int, ) -> ModelType:
+    def get_by_id(
+        self,
+        session: Session,
+        pk: int,
+    ) -> ModelType:
         return session.query(self.model).get(pk)
 
     def create(self, session: Session, obj_in: CreateSchema) -> ModelType:
@@ -45,5 +79,63 @@ class BaseDAO(Generic[ModelType, CreateSchema, UpdateSchema]):
         return session.query(self.model).count()
 
 
-class ArticleDAO(BaseDAO[Article, CreateArticleSchema, UpdateArticleSchema]):
-    model = Article
+class SchemeDAO(BaseDAO[Scheme, CreateScheme, UpdateScheme]):
+    model = Scheme
+
+
+class RequirementAnalysisDAO(
+    BaseDAO[Requirement_analysis, CreateRequirementAnalysis, UpdateRequirementAnalysis]
+):
+    model = Requirement_analysis
+
+
+class RequirementAnalysisDetailDAO(
+    BaseDAO[
+        Requirement_analysis_detail,
+        CreateRequirementAnalysisDetail,
+        UpdateRequirementAnalysisDetail,
+    ]
+):
+    model = Requirement_analysis_detail
+
+
+class SystemFrameworkDAO(
+    BaseDAO[System_framework, CreateSystemFramework, UpdateSystemFramework]
+):
+    model = System_framework
+
+
+class SystemFrameworkDAO(
+    BaseDAO[System_framework, CreateSystemFramework, UpdateSystemFramework]
+):
+    model = System_framework
+
+
+# class SystemFrameworkDetailDAO(BaseDAO[System_framework_detail,CreateSystemFrameworkDetail,UpdateSystemFrameworkDetail]):
+#     model = System_framework_detail
+
+
+class ResponseIndicatorDAO(
+    BaseDAO[Response_indicator, CreateResponseIndicator, UpdateResponseIndicator]
+):
+    model = Response_indicator
+
+
+class ResponseIndicatorDetailDAO(
+    BaseDAO[
+        Response_indicator_detail,
+        CreateResponseIndicatorDetail,
+        UpdateResponseIndicatorDetail,
+    ]
+):
+    model = Response_indicator_detail
+
+
+class BidCatalogDAO(BaseDAO[Bid_catalog, CreateBidCatalog, UpdateBidCatalog]):
+    model = Bid_catalog
+
+
+class BidCatalogContentDAO(
+    BaseDAO[Bid_catalog_content, CreateBidCatalogContent, UpdateBidCatalogContent]
+):
+    model = Bid_catalog_content

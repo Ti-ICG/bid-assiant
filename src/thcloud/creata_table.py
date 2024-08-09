3f3c01d1fa72d1b7ebb391a5ea616091435e0777
ykking-0516 <156627183+ykking-0516@users.noreply.github.com>
2024-08-09 11:41:32 +0800

    Update creata_table.py

--- src/thcloud/creata_table.py
+++ src/thcloud/creata_table.py
@@ -2,6 +2,5 @@ from sqlalchemy.ext.declarative import declarative_base
 
 import thcloud.db as db
 import thcloud.models as models
-from thcloud.models import Article
 
 models.BaseModel.metadata.create_all(bind=db.engine)
